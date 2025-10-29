dc9a4a8911730fd907f85dc1c1d506d0b7147ffd
Rickard Garnau <rickard.garnau89@gmail.com>
2025-10-29 09:39:42 +0100

    update

--- lab_2/rectangle.py
+++ lab_2/rectangle.py
@@ -1,57 +1,86 @@
-from shape import Shape
+from shape import Shape  # Import the "Shape" template from our shape.py file
 
 
+# Our new Rectangle class, which inherits all features from Shape
 class Rectangle(Shape):
-    # The constructor of the Rectangle class initializes the recangle object
+    # This method runs every time we create a new Rectangle object
     def __init__(self, x: float = 0, y: float = 0, width: float = 1, height: float = 1):
-        super().__init__(x, y)  # we reuse the attributes from the Shape class
+        # Call the parent class constructor to save x and y
+        super().__init__(x, y)
+        # Set the width and height. This will call our setters below.
         self.width = width
         self.height = height
 
-    @property
-    def width(self):
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, (int, float)):
-            raise TypeError("The width has to be number")
-        self._width = value
-
-    @property
-    def height(self):
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, (int, float)):
-            raise TypeError("The height has to be number")
-        self._height = value
-
-    @property
+    @property  # A "property" lets us call this method without parentheses (e.g., r.area)
     def area(self):
+        # Calculate the area. Uses _width and _height to get the saved values.
         return self._width * self._height
 
     @property
     def perimeter(self):
+        # Calculate the perimeter
         return 2 * (self._width + self._height)
 
+    @property
+    def is_square(self):
+        # A property to check if the rectangle is actually a square
+        return self._width == self._height  # Returns True if they are equal, else False
+
+    @property
+    def width(self):  # Getter lets us *get* the value (e.g., r.width)
+        return self._width  # Returns the private value we stored
+
+    @width.setter  # Setter runs when we set a value (e.g., r.width = 10)
+    def width(self, value):
+        if not isinstance(value, (int, float)):  # Check if the new value is a number
+            raise TypeError("The width has to be number")  # If not, raise an error
+        if value < 0:  # Also check that the value is not negative
+            raise ValueError("The width cannot be negative")  # If it is, raise an error
+        self._width = (
+            value  # If all checks pass, save the value to the private _width variable
+        )
+
+    @property
+    def height(self):  # Getter for height
+        return self._height
+
+    @height.setter
+    def height(self, value):  # Setter for height
+        if not isinstance(value, (int, float)):  # Same checks as for width
+            raise TypeError("The height has to be number")
+        if value < 0:
+            raise ValueError("The height cannot be negative")
+        self._height = value
+
     def __repr__(self):
-        return f"Rectangle(width={self._width}, height={self._height}, area={self.area}, perimeter={self.perimeter}))"
+        # The formal string for developers. Should look like the code to create the object.
+        return f"Rectangle(x={self.x}, y={self.y}, width={self.width}, height={self.height})"
 
     def __str__(self):
+        # The more readable string used when you run print.
         return f"The area of the rectangle is: {self.area}. The perimeter of the rectangle is: {self.perimeter}"
 
     def find_center(self):
-        # I took inspiration from: https://greenteapress.com/thinkpython/html/thinkpython016.html part 15.4
+        # A method to find the center point of the rectangle
+        # The center = start (x, y) + half the width/height
+        # This method comes from https://greenteapress.com/thinkpython/html/thinkpython016.html part 15.4
         center_x = self.x + self._width / 2.0
         center_y = self.y + self._height / 2.0
-        return (center_x, center_y)
+        return (center_x, center_y)  # Returns the center as a tuple, e.g., (10, 15)
 
 
+# This test code below only runs when we execute this file directly (e.g., by typing "python rectangle.py" in the terminal)
+
+# Create an instance of the class Rectangle.
 r = Rectangle(width=4, height=6)
 
+# Use our properties to print information
 print(f"Width: {r.width}")
 print(f"Height: {r.height}")
 print(f"Area: {r.area}")
 print(f"Perimeter: {r.perimeter}")
+print(f"Is it a square? {r.is_square}")  # Should be False
+
+# Create another rectangle, this one is a square. Idea from 'LLM'
+r2 = Rectangle(width=5, height=5)
+print(f"Is r2 a square? {r2.is_square}")  # Should be True
--- lab_2/shape.py
+++ lab_2/shape.py
@@ -16,7 +16,7 @@ class Shape(ABC):  # The parent class Shape
         if not isinstance(value, (int, float)):
             raise TypeError("'x' must be a number, (int or float)")
         else:
-            self._x = value
+            self._x = value  # If the check passes, save the value.
 
     @property  # getter for y
     def y(self):
@@ -34,43 +34,55 @@ class Shape(ABC):  # The parent class Shape
     def area(self):
         pass
 
-    # @abstractmethod to signal the subclasses that these methods are required else they will get a TypeError
+    # We don't know how to calculate the area or perimeter of a 'Shape',
+    # so we leave it empty with 'pass' as a placeholder.
 
     @property
     @abstractmethod
     def perimeter(self):
         pass
 
-    def __eq__(self, other):  # comparison operator - equal
-        return self.x == other.x and self.y == other.y
+    def __eq__(self, other):  # Compares area
+        if not isinstance(other, Shape):
+            return False
+        return self.area == other.area
 
-    def __lt__(self, other):  # less than, checks x then y
-        if self.x == other.x:
-            return self.y < other.y
-        return self.x < other.x
+    def __lt__(self, other):  # Less than
+        if not isinstance(other, Shape):
+            return NotImplemented  # https://docs.python.org/3/reference/datamodel.html
+        return self.area < other.area
 
-    def __le__(self, other):  # less than or equal to
-        if self.x == other.x:
-            return self.y <= other.y
-        return self.x < other.x
+    def __le__(self, other):  # Less than or equal to
+        if not isinstance(other, Shape):
+            return NotImplemented
+        return self.area <= other.area
 
-    def __gt__(self, other):  # greater than
-        if self.x == other.x:
-            return self.y > other.y
-        return self.x > other.x
+    def __gt__(self, other):  # Greater than
+        if not isinstance(other, Shape):
+            return NotImplemented
+        return self.area > other.area
 
-    def __ge__(self, other):  # greater or equal to
-        if self.x == other.x:
-            return self.y >= other.y
-        return self.x > other.x
+    def __ge__(self, other):  # Greater or equal to
+        if not isinstance(other, Shape):
+            return NotImplemented
+        return self.area >= other.area
 
     # I got help and inspiration for the overloading comparison operators from:
     # https://docs.python.org/3/reference/datamodel.html#special-method-names
 
-    def __repr__(
-        self,
-    ):  # formal representation for reviewing the object. https://www.geeksforgeeks.org/python/python-program-to-get-the-class-name-of-an-instance/
+    # self.__class__.__name__ is a clever way to get the *actual* class name,
+    # instead of just "Shape". Used by developers review the code, debug.
+    # https://www.geeksforgeeks.org/python/python-program-to-get-the-class-name-of-an-instance/
+    def __repr__(self):
         return f"{self.__class__.__name__}, (x={self.x}, y={self.y})"
 
-    def __str__(self):  # clean representation for viewing
+    def __str__(self):  # clean representation for viewers
         return f"Shape at position ({self.x}, {self.y})"
+
+    def translate(self, dx, dy):  # a method to move the shape around.
+        # check if the values are numeric before moving
+        if not isinstance(dx, (int, float)) or not isinstance(dy, (int, float)):
+            raise TypeError("Translate values must be numbers (int or float)")
+        # if numeric add to x an y.
+        self.x += dx
+        self.y += dy
